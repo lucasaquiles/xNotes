d0b7cc4ad5272d019da3bd3163dee2b780600509
lucasaquiles <lucasaquiles@gmail.com>
2011-12-30 18:38:54 -0200

    externalização dos js e criação dinâmica dos componentes

--- xNotes.js
+++ xNotes.js
@@ -1,13 +1,38 @@
+window.localStorage["prioridadeList"] = new Array('normal','baixa','urgente','alta');
 
 
-function addNote(note){
+function initComponents(){
 	
-	localStorage["task"] = note;
+	mountSelectPrioridades();
+	mountCompromissos();
 }
 
-function listNotes(){
-	
-	
+function mountCompromissos(){
+		
+	$("#compromissosList").html(localStorage.getItem('compromissos'));
 }
 
+function mountSelectPrioridades(){
+	
+	
+	size = window.localStorage.getItem('prioridadeList').split(',').length;
+	prioridades = window.localStorage.getItem('prioridadeList').split(',');
+	
+	for(i = 0; i < size; i++){
+	    optionTag = "<option value='"+i+"'>"+prioridades[i]+"</option>";		    	
+	    $('#priority').prepend(optionTag);
+	   
+	}
+}
 
+function addNote(task){
+	
+	if(localStorage.setItem('compromissos') != 'undefined'){
+		priority = $('select#priority option:selected').text();
+		tag = "<li class='to-do'>"+task+"- <span class='priority'>"+priority+"</span></li>";
+		$("#compromissosList").append(tag);
+		window.localStorage.setItem('compromissos', $("#compromissosList").html());
+	}
+	
+	return false;
+}
