1708b9aeca69a0dda8c521d81c40ec656f899d7c
lucasaquiles <lucasaquiles@gmail.com>
2012-01-09 22:34:52 -0200

    ajuste no tamanho do popup

--- xNotes.js
+++ xNotes.js
@@ -14,22 +14,33 @@ function mountCompromissos(){
 
 function mountSelectPrioridades(){
 	
-	
 	size = window.localStorage.getItem('prioridadeList').split(',').length;
 	prioridades = window.localStorage.getItem('prioridadeList').split(',');
 	
 	for(i = 0; i < size; i++){
 	    optionTag = "<option value='"+i+"'>"+prioridades[i]+"</option>";		    	
 	    $('#priority').prepend(optionTag);
-	   
 	}
 }
 
 function addNote(task){
 	
 	if(localStorage.setItem('compromissos') != 'undefined'){
+	
+		
+		
+		div = "<div style='display:none;'>"+
+				"<form>"+
+					"<div><input type='radio' id='status' name='status' value='to-do'> to do</div>"+
+					"<div><input type='radio' id='status' name='status' value='doing'> doing</div>"+
+					"<div><input type='radio' id='status'name='status' value='done'> done</div>"+
+					"<div><input type='submit' value='ok'>"+
+				"</form>"+
+			"</div>";
+	
 		priority = $('select#priority option:selected').text();
-		tag = "<li class='to-do'>"+task+"- <span class='priority'>"+priority+"</span></li>";
+		tag = "<li class='to-do'>"+task+" - <span class='priority'>"+priority+"</span>"+div+"</li>";
+		
 		$("#compromissosList").append(tag);
 		window.localStorage.setItem('compromissos', $("#compromissosList").html());
 	}
